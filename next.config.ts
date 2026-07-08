8d5afcbf4b18b77ef3432108b23b73fc0795ab03
Shivam Mishra <shivam6390mishra@gmail.com>
2026-07-08 16:23:22 +0530

    Remove localhost URLs

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/api/:path*",
+        destination: "https://prachinx-backend.onrender.com/api/:path*",
       },
     ];
   },
